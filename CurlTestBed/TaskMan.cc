52141896b59bfd4e075dfafa37881aacaf080510
eminom <hai.bai.cn@gmail.com>
2015-08-04 18:48:45 +0800

    deploy one file.

--- CurlTestBed/TaskMan.cc
+++ CurlTestBed/TaskMan.cc
@@ -3,11 +3,27 @@
 #include "TaskMan.h"
 #include "TaskItemBase.h"
 
+#include <cstring>
+#include <cstdlib>
+
 #ifdef _MSC_VER
 
 #include <windows.h>
+#include <shlobj.h>
 
 void DeployOneFile(const char *from, const char *to){
+	const char* rh = strrchr(to, '/');
+	if(rh){
+		char subpath[BUFSIZ] = "";
+		strncpy(subpath, to, rh - to);
+		char cur[MAX_PATH+1];
+		GetCurrentDirectoryA(MAX_PATH, cur);
+		strcat(cur, "/");
+		strcat(cur, subpath);
+		SECURITY_ATTRIBUTES sa={sizeof(SECURITY_ATTRIBUTES)};
+		int res = SHCreateDirectoryExA(NULL, cur, &sa);
+		//printf("Creating result = %d\n", res);
+	}
 	CopyFileA(from, to, FALSE);
 }
 
