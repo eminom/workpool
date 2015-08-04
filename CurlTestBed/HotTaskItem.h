6e80b572e8da7a71113f9d2bb2d027836db92137
eminom <hai.bai.cn@gmail.com>
2015-08-04 13:53:48 +0800

    update

---
+++ CurlTestBed/HotTaskItem.h
@@ -0,0 +1,41 @@
+
+
+#ifndef _HOT_TASK_ITEM__DEF__
+#define _HOT_TASK_ITEM__DEF__
+
+#include <cstdio>
+#include <string>
+
+class HotTaskItem {
+public:
+	HotTaskItem(const char *line, const char *baseUrl)
+		:ok_(false),
+		rawEntry_(line),
+		baseUrl_(baseUrl) {
+		int length = 0;
+		char md5name[BUFSIZ], xxhash[BUFSIZ], path[BUFSIZ];
+		if( 4 == sscanf(line, "%s%d%s%s", md5name, &length, xxhash, path)){
+			ok_ = true;
+			length_  = length;
+			md5name_ = md5name;
+			path_    = path;
+			xxhash_  = xxhash;
+		}
+	}
+	operator bool()const{
+		return ok_;
+	}
+private:
+	bool ok_;
+public:
+	std::string rawEntry_;
+	std::string md5name_;
+	std::string xxhash_;
+	std::string path_;
+	std::string baseUrl_;
+	int length_;
+};
+
+
+
+#endif
---
+++ CurlTestBed/xxhashwrapper.h
@@ -0,0 +1,7 @@
+
+#ifndef _DEFINES__XXHASH__WRAPPER__
+#define _DEFINES__XXHASH__WRAPPER__
+
+unsigned int calculateFileXXHASH(const char *path);
+
+#endif
