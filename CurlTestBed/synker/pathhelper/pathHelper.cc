e96b71622e166b344de02b5cadd3d7662b3ef6b5
eminom <hai.bai.cn@gmail.com>
2015-08-07 14:12:38 +0800

    path helper.

--- CurlTestBed/synker/pathhelper/pathHelper.cc
+++ CurlTestBed/synker/pathhelper/pathHelper.cc
@@ -90,7 +90,7 @@ std::string PathHelper::formatResourceUri(HotTaskItem *pItem){
 }
 
 
-void PathHelper::DeployOneFile(const char *from, const char *to){
+bool PathHelper::DeployOneFile(const char *from, const char *to){
     std::string pre = PathHelper::getInstance().getWritablePath();
     std::string source = pre + from;
     std::string target = pre + to;
@@ -99,7 +99,7 @@ void PathHelper::DeployOneFile(const char *from, const char *to){
         std::string subdir = target.substr(0, pos);
 		create_path(subdir);
     }
-	CopyFileA(source.c_str(), target.c_str(), FALSE);
+	return !!CopyFileA(source.c_str(), target.c_str(), FALSE);
 }
      
 
