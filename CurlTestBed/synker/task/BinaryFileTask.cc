9e03ad37c92aecb719e5fc276586c7393142a5fe
eminom <hai.bai.cn@gmail.com>
2015-08-19 14:08:35 +0800

    save and store.

--- CurlTestBed/synker/task/BinaryFileTask.cc
+++ CurlTestBed/synker/task/BinaryFileTask.cc
@@ -12,18 +12,11 @@ BinaryFileTask::BinaryFileTask(const char *url, const char *save, int cap)
 void BinaryFileTask::onFinalized(CURLcode res) {
 	BinaryStreamTask::onFinalized(res);
 	if(0==res) {
-		int length = write_ptr_ - begin_ptr_;
         //----- NEED THE ABSOLUTE PATH NOW.
         std::string prepath = PathHelper::getInstance().getWritablePath();
         //prepath.append("/");
         prepath.append(_save);
-		if( FILE *fout = fopen(prepath.c_str(), "wb") ) {
-			fwrite(begin_ptr_, 1, length, fout);
-			fclose(fout);
-			//printf("binary stream saved to %s\n", _save.c_str());
-		} else {
-			fprintf(stderr, "Failed to open %s\n", _save.c_str());
-		}
+		saveToPath(prepath.c_str());
 	} else {
 		fprintf(stderr,"Failed to download %s\n", _save.c_str());
 	}
